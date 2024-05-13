b9a35348f13d23e0849cd5e4f746b9427f7a4005
rayendlimi <rayen.dlimi0107@gmail.com>
2024-05-13 23:39:46 +0000

    "Modifying the VinylMix Entity and make migrations (V2)

--- migrations/Version20240513195116.php
+++ migrations/Version20240513195116.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240507154327 extends AbstractMigration
+final class Version20220718170741 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20240507154327 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE vinyl_mix (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, track_count INT NOT NULL, genre VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE vinyl_mix ADD votes INT NOT NULL');
     }
 
     public function down(Schema $schema): void
